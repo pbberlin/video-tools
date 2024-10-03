f08cc5f62470861806b65cb9d600d1257b43c86b
Peter Buchmann <peter.buchmann@web.de>
2024-10-03 12:15:12 +0200

    dir a param to conv2h256.sh

--- conv2h256-dir.sh
+++ conv2h256-dir.sh
@@ -1,6 +1,16 @@
-# "C:\Program Files\Git\git-bash.exe"
-# ./conv2h256-dir.sh
-for filename in ./*.mkv; do
+#!/bin/bash
+
+directory="$1"
+
+if [[ -z "$directory" ]]; then
+        echo "work dir - first arg - missing "
+        exit 1
+else
+        echo "converting dir $directory to h.256"
+fi
+
+
+for filename in "$directory"/*.mkv; do
         echo "Re-coding -$filename- to h.256"
         ffmpeg -i "$filename" -c:v libx265 -vtag hvc1 "$filename.mp4"
 done
