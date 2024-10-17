5506763cdd016f1b0e5a3a09925fb5d48c75229d
Peter Buchmann <peter.buchmann@web.de>
2024-10-17 22:55:11 +0200

    tiny

--- conv2h256-dir.sh
+++ conv2h256-dir.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-directory="$1"
+dir="$1"
 
-if [[ -z "$directory" ]]; then
+if [[ -z "$dir" ]]; then
         echo "work dir - first arg - missing "
         exit 1
 else
-        echo "converting dir $directory to h.256"
+        echo "converting dir $dir to h.256"
+        echo "  "
 fi
 
 
-for filename in "$directory"/*.mkv; do
-        echo "Re-coding -$filename- to h.256"
-        ffmpeg -i "$filename" -c:v libx265 -vtag hvc1 "$filename.mp4"
+# first  extension
+for filename in "${dir}"/*.mp4 ; do
+        flnBase=$(basename "$filename" .mp4)
+        echo "re-coding -${flnBase}- to h.256"
+        ffmpeg -i "$filename" -c:v libx265 -vtag hvc1 "$flnBase.256.mp4"
 done
+
+# second extension
+for filename in "${dir}"/*.mkv ; do
+        flnBase=$(basename "$filename" .mkv)
+        echo "re-coding -${flnBase}- to h.256"
+        ffmpeg -i "$filename" -c:v libx265 -vtag hvc1 "$flnBase.256.mp4"
+done
+
