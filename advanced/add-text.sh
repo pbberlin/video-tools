ca2f85eb18267c48999c2a11429368b78a091ae6
Peter Buchmann <peter.buchmann@web.de>
2024-11-17 00:29:48 +0100

    subs

--- advanced/add-text.sh
+++ advanced/add-text.sh
@@ -16,6 +16,24 @@ ffmpeg -i with-audio.mp4   \
 
 
 
+ffmpeg -i wolve.mp4   \
+  -vf "drawtext=text='1':fontfile=C\\\\Windows\\\\Fonts\\\\GOTHIC.TTF:fontcolor=white:fontsize=32:x=(w-text_w)/1-20:y=(h-text_h)-12:bordercolor=white:borderw=2:enable='between(t,0.0,1.92)'  , \
+       drawtext=text='2':fontfile=C\\\\Windows\\\\Fonts\\\\GOTHIC.TTF:fontcolor=white:fontsize=32:x=(w-text_w)/1-20:y=(h-text_h)-12:bordercolor=white:borderw=2:enable='between(t,3.0,4.5)'   , \
+       drawtext=text='3':fontfile=C\\\\Windows\\\\Fonts\\\\GOTHIC.TTF:fontcolor=white:fontsize=32:x=(w-text_w)/1-20:y=(h-text_h)-12:bordercolor=white:borderw=2:enable='between(t,4.7,6.4)'   , \
+       drawtext=text='4':fontfile=C\\\\Windows\\\\Fonts\\\\GOTHIC.TTF:fontcolor=white:fontsize=32:x=(w-text_w)/1-20:y=(h-text_h)-12:bordercolor=white:borderw=2:enable='between(t,6.55,7.8)'  , \
+       drawtext=text='5':fontfile=C\\\\Windows\\\\Fonts\\\\GOTHIC.TTF:fontcolor=white:fontsize=32:x=(w-text_w)/1-20:y=(h-text_h)-12:bordercolor=white:borderw=2:enable='between(t,8.0,9.4)'   , \
+       drawtext=text='6':fontfile=C\\\\Windows\\\\Fonts\\\\GOTHIC.TTF:fontcolor=white:fontsize=32:x=(w-text_w)/1-20:y=(h-text_h)-12:bordercolor=white:borderw=2:enable='between(t,9.55,11.4)' , \
+       drawtext=text='7':fontfile=C\\\\Windows\\\\Fonts\\\\GOTHIC.TTF:fontcolor=white:fontsize=32:x=(w-text_w)/1-20:y=(h-text_h)-12:bordercolor=white:borderw=2:enable='between(t,11.5,12.8)' , \
+       drawtext=text='8':fontfile=C\\\\Windows\\\\Fonts\\\\GOTHIC.TTF:fontcolor=white:fontsize=32:x=(w-text_w)/1-20:y=(h-text_h)-12:bordercolor=white:borderw=2:enable='between(t,12.9,14.9)' , \
+       drawtext=text='9':fontfile=C\\\\Windows\\\\Fonts\\\\GOTHIC.TTF:fontcolor=white:fontsize=32:x=(w-text_w)/1-20:y=(h-text_h)-12:bordercolor=white:borderw=2:enable='between(t,15.0,17.5)' , \
+  " \
+  -c:a copy \
+  -c:v libx264 \
+  output-with-subs.mp4
+
+
+
+
 # The kyrillic chars did not render
 
 export LANG="en_US.UTF-8"
